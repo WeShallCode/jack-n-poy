9a1807c754d44605fba2ea34c2312124340078b1
Kent Nehemiah Ybalez <kent.ybalez37@gmail.com>
2017-09-11 21:19:54 +0800

    Updated some things

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # jack-n-poy
-A simple code of the game Rock, Paper, and Scissor (Jack 'N Poy)
-Written in Python 3. 
+
+## About this Repo
+
+A simple code for the game Rock, Paper, and Scissor (Jack 'N Poy).
+Written in Python 3.
+
+## Notes
 
 This is written as a test file for my studies in Python 3. 
+
+This file will evolve as I learn more about Python programming.
--- rpsGame.py
+++ rpsGame.py
@@ -1,3 +1,5 @@
+import sys
+import msvcrt as msv
 from random import choice
 
 movesList = ['Rock','Paper', 'Scissor']
@@ -15,21 +17,68 @@ def userMenu():
     print('------------------------------')
     print('Menu: \n[0] Rock \n[1] Paper \n[2] Scissor')
     userChoice = int(input('Choice: '))
-    gameProcess(userChoice)
+
+    if userChoice > 2:
+        print("Wrong choice! Please choose again.")
+        userMenu()
+
+    else:
+        gameProcess(userChoice)
+
+
+# Another menu
+def continueGame():
+    print('------------------------------')
+    print("Continue playing? \nPress [P] to play again. Press ENTER to EXIT.")
+
+    keyPress = ord(msv.getch())
+
+    if keyPress == 80 or keyPress == 112:
+        userMenu()
+    elif keyPress == 13:
+        print("Thank you for playing!")
+        sys.exit()
+
+
+# Score function
+def postScore(user = 0, computer = 0):
+    global userScore
+    global computerScore
+
+    print('------------------------------')
+    
+    if user == 0 and computer == 0:
+        print("SCORE: \nUser: ", userScore, "\nComputer: ", computerScore)
+
+    elif user == 1:
+        userScore += 1
+        print("SCORE: \nUser: ", userScore, "\nComputer: ", computerScore)
+        
+    else:
+        computerScore += 1
+        print("SCORE: \nUser: ", userScore, "\nComputer: ", computerScore)
 
 
 # Process
 def gameProcess(userChoice):
     computerChoice = computerMove()
 
+    print('------------------------------')
+
     if userChoice == computerChoice:
         print("RESULT: It's a TIE!")
+        postScore()
+        continueGame()
 
     elif userChoice - computerChoice == 1 or userChoice - computerChoice == -2:
         print("RESULT: User WIN, Computer LOOSE!")
+        postScore(1,0)
+        continueGame()
 
     else:
         print("RESULT: User LOOSE, Computer WIN!")
+        postScore(0,1)
+        continueGame()
 
 
 # Main
